c94938c844d176583845999b23e9d71a7d811e96
MarneusAugustusCalgar1989 <egor@westsib.ru>
2024-07-23 15:15:24 +0700

    Audio converter interface added

--- src/components/AudioConverter.jsx
+++ src/components/AudioConverter.jsx
@@ -1,12 +1,82 @@
-import Wrapper from './Wrapper'
-const AudioConverter = () => {
+import { useState } from 'react';
+import Wrapper from './Wrapper';
+import { useNavigate } from 'react-router-dom';
+const AudioConverter = e => {
+  const navigate = useNavigate();
+  const [soundUpload, setSoundUpload] = useState();
+  const [audioName, setAudioName] = useState();
+  const [showInput, setShowInput] = useState(true);
+
+  const sendAudio = async e => {
+    e.preventDefault();
+    setShowInput(false);
+
+    let filename = e.target.filename.files[0].name;
+    filename = 'to_' + filename.slice(0, filename.length - 4) + '.mp3';
+    setAudioName(filename);
+    const formData = new FormData();
+    formData.append('file', e.target.filename.files[0]);
+    const response = await fetch('http://213.59.156.172:3000/convert_audio', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      const reader = response.body.getReader();
+      let chunks = [];
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          break;
+        }
+        chunks.push(value);
+      }
+      const blob = new Blob(chunks, { type: 'audio/webm; codecs=opus' });
+      const converterdFile = URL.createObjectURL(blob);
+      console.log('All is ok');
+      URL.revokeObjectURL(blob);
+      setSoundUpload(converterdFile);
+      await fetch('http://213.59.156.172:3000/cleaner_for_audio', {
+        method: 'POST',
+      });
+      return converterdFile;
+    } else {
+      console.log(response.status, response.statusText);
+      navigate('/');
+    }
+  };
   return (
-    <div className="App">
+    <div className='App'>
       <Wrapper>
         <h1>Audio Converter</h1>
+        {soundUpload && (
+          <>
+            <a
+              href={soundUpload}
+              download={audioName}
+              className='image_converter_result'
+            >
+              <p>Download file</p>
+            </a>
+            <button
+              onClick={() => {
+                setSoundUpload();
+                setShowInput(true);
+              }}
+            >
+              Попробовать еще раз
+            </button>
+          </>
+        )}
+        {showInput && (
+          <form onSubmit={sendAudio}>
+            <input type='file' name='filename' />
+            <button type='submit'> Отправить аудио </button>
+          </form>
+        )}
       </Wrapper>
     </div>
-  )
-}
+  );
+};
 
-export default AudioConverter
+export default AudioConverter;
--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,36 +1,30 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const MainMenu = ({ children }) => {
-  const currentUrl = window.location.href
-  console.log(currentUrl)
+  const currentUrl = window.location.href;
+  console.log(currentUrl);
   return (
     <main>
-      <div className="top_container">
-        <div className="container">
-          <h1 className="main_header_top">ВСПОМОГАТОР</h1>
+      <div className='top_container'>
+        <div className='container'>
+          <h1 className='main_header_top'>ВСПОМОГАТОР</h1>
         </div>
-        <div className="menu_list">
+        <div className='menu_list'>
           <ul>
             <li>
-              <Link to="logotyper">Логотипер</Link>
+              <Link to='logotyper'>Логотипер</Link>
             </li>
             <li>
-              <Link to="audioconverter">Звукодел</Link>
-            </li>
-            <li>
-              <Link to="mockuper">Мокапошная</Link>
-            </li>
-            <li>
-              <Link to="documentcreator">Документодел</Link>
+              <Link to='audioconverter'>Звукодел</Link>
             </li>
           </ul>
         </div>
-        <div className="container">
-          <h1 className="main_header_bottom">ВСПОМОГАТОР</h1>
+        <div className='container'>
+          <h1 className='main_header_bottom'>ВСПОМОГАТОР</h1>
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default MainMenu
+export default MainMenu;
