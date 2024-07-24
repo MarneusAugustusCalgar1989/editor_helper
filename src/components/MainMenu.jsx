2877c48594f9915ac79a6e5142f86bf80bd04b0a
MarneusAugustusCalgar1989 <egor@westsib.ru>
2024-07-24 14:00:00 +0700

    Аdd useeffect in main menu for fetching server at first launch

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,7 +1,30 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const MainMenu = ({ children }) => {
   const currentUrl = window.location.href;
+  const [serviceON, setServiceOn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const testFetch = async () => {
+      try {
+        await fetch('http://213.59.156.172:3000/cleaner_for_img', {
+          method: 'POST',
+        }).then(data => (data ? setServiceOn(true) : setServiceOn(false)));
+      } catch (e) {
+        console.log(e.text);
+      } finally {
+        console.log('Fetched');
+      }
+    };
+
+    testFetch();
+  }, []);
+  const reloadWind = () => {
+    window.location.reload();
+  };
+
   return (
     <main>
       <div className='top_container'>
@@ -9,14 +32,20 @@ const MainMenu = ({ children }) => {
           <h1 className='main_header_top'>ВСПОМОГАТОР</h1>
         </div>
         <div className='menu_list'>
-          <ul>
-            <li>
-              <Link to='logotyper'>Логотипер</Link>
-            </li>
-            <li>
-              <Link to='audioconverter'>Звукодел</Link>
-            </li>
-          </ul>
+          {serviceON ? (
+            <ul>
+              <li>
+                <Link to='logotyper'>Логотипер</Link>
+              </li>
+              <li>
+                <Link to='audioconverter'>Звукодел</Link>
+              </li>
+            </ul>
+          ) : (
+            <ul onClick={reloadWind}>
+              <li>Сервис временно недоступен</li>
+            </ul>
+          )}
         </div>
         <div className='container'>
           <h1 className='main_header_bottom'>ВСПОМОГАТОР</h1>
